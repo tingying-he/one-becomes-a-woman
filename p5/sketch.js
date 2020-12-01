5e2a95265581fadccd563f09dd6ba49416ff0f9c
liubq7 <liubq97@gmail.com>
2020-12-02 00:04:42 +0800

    resolve #7

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,53 +1,53 @@
 // Global Vars
 var reset = true;
 var eyes = [];
-var numEyes = 100;
+var numEyes = 5000;
 let eyeImg;
 let femaleImg;
 var female;
 let pg;//mouse trace
 
-let changeTime=0;
+let changeTime = 0;
 
 
 
 function preload() {
-  eyeImg = loadImage('images/eye.png');   
-  femaleImg0 = loadImage('images/female0.png');
-  femaleImg1 = loadImage('images/female2.png');
+    eyeImg = loadImage('images/eye.png');
+    femaleImg0 = loadImage('images/female0.png');
+    femaleImg1 = loadImage('images/female2.png');
 }
 
 // Setup
 function setup() {
     createCanvas(1200, 800);
-    colorMode(RGB,255,255,255,1);
-  
+    // colorMode(RGB, 255, 255, 255, 1);
+
     // Set Background
     //background(0, 0, 0);
 
     // Create Eyes
-    for (i = 0; i < numEyes; i++) {    
-     var x = random(width);
-     var y = random(height);      
-     eyes[i] = new Eye(x, y);
+    for (i = 0; i < numEyes; i++) {
+        var x = random(width);
+        var y = random(height);
+        eyes[i] = new Eye(x, y);
     }
 
     female = new Female(femaleImg0);
-  
+
     pg = createGraphics(1000, 1000);
 }
 
 // Particle
 function Eye(x, y) {
-  this.x = x;
-  this.y = y;
+    this.x = x;
+    this.y = y;
 
     // Properties
-    this.size = random(5,15);
+    this.size = random(20, 50);
     this.maxSpeed = random(5, 10);
-    this.maxForce = random(0.01,4);
+    this.maxForce = random(0.01, 4);
     this.mass = this.size; //this.size * this.size * PI;
-  
+
     // Motion
     this.pos = createVector(x, y);
     this.acc = createVector(0, 0);
@@ -55,15 +55,15 @@ function Eye(x, y) {
     this.vel = createVector(random(-initV, initV), random(-initV, initV));
 
     // External Forces
-    this.applyForce = function(force) {
+    this.applyForce = function (force) {
         var f = force.copy();
         f.div(this.mass);
         this.acc.add(f);
     }
 
     // Behaviors
-    this.seek = function(target) {
-      var desired = p5.Vector.sub(target, this.pos);
+    this.seek = function (target) {
+        var desired = p5.Vector.sub(target, this.pos);
         desired.setMag(this.maxSpeed);
         var steering = p5.Vector.sub(desired, this.vel);
         steering.limit(this.maxForce);
@@ -71,27 +71,27 @@ function Eye(x, y) {
     }
 
     // Handle Updates
-    this.update = function() {
+    this.update = function () {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.set(0, 0);
     }
-  
-  
+
+
 
     // Draw
-    this.display = function() {
-        
+    this.display = function () {
+
         push();
-      
+
         translate(this.pos.x, this.pos.y);
         imageMode(CENTER);
-        image(eyeImg,this.size,this.size,100,100);
+        image(eyeImg, this.size, this.size, this.size, this.size);
         pop();
     }
 
-   // Handle Edges
-    this.edges = function() {
+    // Handle Edges
+    this.edges = function () {
         if (this.pos.y > height) {
             this.pos.y = 0 - this.size;
         } else if (this.pos.y < 0) {
@@ -105,47 +105,47 @@ function Eye(x, y) {
     }
 }
 //draw body
-function Female(img){ 
-    this.display = function(img){
+function Female(img) {
+    this.display = function (img) {
         console.log(img);
         image(img, 500, 0, 290, 792);
         console.log("Female is called");
     }
-   
+
 }
 
 // Update Canvas
 function draw() {
     clear();
-  
-    background(0,0,0);
+
+    background(0, 0, 0);
     console.log(femaleImg0);
     female.display(femaleImg0);
 
     let currentTime = millis();
     console.log(currentTime);
     // let timeInterval = currentTime-changeTime;
-    if( currentTime > 5000 ){//&& currentTime <10000
+    if (currentTime > 5000) {//&& currentTime <10000
         female.display(
             femaleImg1
-            );    
+        );
         // changeTime = currentTime;
     }
-    
+
     // console.log(female.display());
-  
-  pg.stroke(255,255,0)
-  pg.line(mouseX, mouseY, pmouseX, pmouseY);
-  image(pg, 0, 0);
-  
+
+    pg.stroke(255, 255, 0)
+    pg.line(mouseX, mouseY, pmouseX, pmouseY);
+    image(pg, 0, 0);
+
 
     var target = createVector(mouseX, mouseY);
-    for (i = 0; i < numEyes; i++) {
+    for (i = 0; i < currentTime / 100 * 3; i++) {
         eyes[i].seek(target);
         eyes[i].display();
         eyes[i].update();
     }
-  
+
 }
 
 
