69ccc76b0a87ca986173569bc250bf1aeae4d9c5
tingying-he <tingying.he@outlook.com>
2020-12-04 00:10:17 +0100

    background

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,150 +1,144 @@
 // Global Vars
 var reset = true;
-var eyes = [];
-var numEyes = 5000;
-let eyeImg;
-let femaleImg;
-var female;
-let pg;//mouse trace
-
+var a = 0;
 let changeTime = 0;
 
+var streams = [];
+var fadeInterval = 1.6;
+var symbolSize = 14;
+var comments = [
+                "1He gives his harness bells a shake",
+                "2To ask if there is some mistake.",
+                "3The only other sound’s the sweep",
+                "4Of easy wind and downy flake.",
+                "5The woods are lovely, dark, and deep,",
+                "6But I have promises to keep,",
+                "7And miles to go before I sleep,"
+                ];
 
-
-function preload() {
-    eyeImg = loadImage('images/eye.png');
-    femaleImg0 = loadImage('images/female0.png');
-    femaleImg1 = loadImage('images/female2.png');
-}
 
 // Setup
 function setup() {
-    createCanvas(1200, 800);
-    // colorMode(RGB, 255, 255, 255, 1);
+    createCanvas(window.innerWidth, window.innerHeight);
 
-    // Set Background
-    //background(0, 0, 0);
+    background(0);
 
-    // Create Eyes
-    for (i = 0; i < numEyes; i++) {
-        var x = random(width);
-        var y = random(height);
-        eyes[i] = new Eye(x, y);
+    var y = 0;
+    for (var i = 0; i <= height / symbolSize; i++) {
+        var stream = new Stream();
+        // stream.generateSymbols(random(-2000, 0), y);
+        stream.generateSymbols(random(0, 2000), y);
+        streams.push(stream);
+        y += symbolSize
     }
 
-    female = new Female(femaleImg0);
-
-    pg = createGraphics(1000, 1000);
+    // textFont('Consolas');
+    // textSize(symbolSize);
 }
 
-// Particle
-function Eye(x, y) {
+
+function Symbol(x, y, speed, first, opacity) {
     this.x = x;
     this.y = y;
+    // this.value;
 
-    // Properties
-    this.size = random(20, 50);
-    this.maxSpeed = random(5, 10);
-    this.maxForce = random(0.01, 4);
-    this.mass = this.size; //this.size * this.size * PI;
+    this.speed = speed;
+    this.first = first;
+    this.opacity = opacity;
 
-    // Motion
-    this.pos = createVector(x, y);
-    this.acc = createVector(0, 0);
-    var initV = 30;
-    this.vel = createVector(random(-initV, initV), random(-initV, initV));
+    // this.switchInterval = round(random(2, 25));
 
-    // External Forces
-    this.applyForce = function (force) {
-        var f = force.copy();
-        f.div(this.mass);
-        this.acc.add(f);
+    // this.setToRandomSymbol = function () {
+    //     var charType = round(random(0, 5));
+    //     if (frameCount % this.switchInterval == 0) {
+    //         if (charType > 1) {
+    //             // set it to Katakana
+    //             this.value = String.fromCharCode(
+    //                 0x30A0 + floor(random(0, 97))
+    //             );
+    //         } else {
+    //             // set it to numeric
+    //             this.value = floor(random(0, 10));
+    //         }
+    //     }
+    // }
+
+    this.setToRandomSymbol = function(){
+        // var a = round(random(0,comments.length-1))
+        this.value = comments[a%6]
+        a = a + 1;
+        // console.log(a);
     }
 
-    // Behaviors
-    this.seek = function (target) {
-        var desired = p5.Vector.sub(target, this.pos);
-        desired.setMag(this.maxSpeed);
-        var steering = p5.Vector.sub(desired, this.vel);
-        steering.limit(this.maxForce);
-        this.applyForce(steering);
-    }
-
-    // Handle Updates
-    this.update = function () {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.set(0, 0);
-    }
-
-
-
-    // Draw
-    this.display = function () {
-
-        push();
-
-        translate(this.pos.x, this.pos.y);
-        imageMode(CENTER);
-        image(eyeImg, this.size, this.size, this.size, this.size);
-        pop();
-    }
-
-    // Handle Edges
-    this.edges = function () {
-        if (this.pos.y > height) {
-            this.pos.y = 0 - this.size;
-        } else if (this.pos.y < 0) {
-            this.pos.y = height + this.size;
-        }
-        if (this.pos.x > width + this.size) {
-            this.pos.x = 0 - this.size;
-        } else if (this.pos.x < 0 - this.size) {
-            this.pos.x = width + this.size;
-        }
-    }
-}
-//draw body
-function Female(img) {
-    this.display = function (img) {
-        console.log(img);
-        image(img, 500, 0, 290, 792);
-        console.log("Female is called");
+    this.rain = function () {
+        this.x = (this.x >= width) ? 0 : this.x += this.speed;
     }
 
 }
+
+function Stream() {
+    this.symbols = [];
+    // this.totalSymbols = round(random(20, 50));
+    // this.totalSymbols = 0;
+    this.speed = random(1,3); 
+
+    this.generateSymbols = function (x, y) {
+        var opacity = 255;
+        var first = round(random(0, 4)) == 1;
+        symbol = new Symbol(
+            x,
+            y,
+            this.speed,
+            first,
+            opacity
+        );
+        symbol.setToRandomSymbol();
+        this.symbols.push(symbol);
+        // opacity -= (255 / this.totalSymbols) / fadeInterval;
+        // x -= symbolSize;
+        // first = false;
+        // for (var i = 0; i <= this.totalSymbols; i++) {
+        //     symbol = new Symbol(
+        //         x,
+        //         y,
+        //         this.speed,
+        //         first,
+        //         opacity
+        //     );
+        //     symbol.setToRandomSymbol();
+        //     this.symbols.push(symbol);
+        //     opacity -= (255 / this.totalSymbols) / fadeInterval;
+        //     x -= symbolSize;
+        //     first = false;
+        // }
+
+    }
+
+    this.render = function () {
+        
+        this.symbols.forEach(function (symbol) {
+            if (symbol.first) {
+                fill(134, 128, 128, symbol.opacity);  // 134, 128, 128,
+            } else {
+                fill(92, 79, 79, symbol.opacity);  // 92, 79, 79,
+            }
+            text(symbol.value, symbol.x, symbol.y);
+            symbol.rain();
+            // symbol.setToRandomSymbol();
+        });
+    }
+}
+
+
 
 // Update Canvas
 function draw() {
-    clear();
 
-    background(0, 0, 0);
-    console.log(femaleImg0);
-    female.display(femaleImg0);
+    background(0, 150);
 
-    let currentTime = millis();
-    console.log(currentTime);
-    // let timeInterval = currentTime-changeTime;
-    if (currentTime > 5000) {//&& currentTime <10000
-        female.display(
-            femaleImg1
-        );
-        // changeTime = currentTime;
-    }
-
-    // console.log(female.display());
-
-    pg.stroke(255, 255, 0)
-    pg.line(mouseX, mouseY, pmouseX, pmouseY);
-    image(pg, 0, 0);
-
-
-    var target = createVector(mouseX, mouseY);
-    for (i = 0; i < currentTime / 100 * 3; i++) {
-        eyes[i].seek(target);
-        eyes[i].display();
-        eyes[i].update();
-    }
+    streams.forEach(function (stream) {
+        stream.render();
+    });
 
 }
 
