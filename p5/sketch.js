81b0f0f6ae38ee3591bb7ec9151d3d6841563a6e
liubq7 <liubq97@gmail.com>
2020-12-10 15:19:52 +0800

    fix lib error

--- p5/sketch.js
+++ p5/sketch.js
@@ -11,25 +11,23 @@ let pg;//mouse trace
 let changeTime=0;
 let whisper;
 
-// /* comments */
-// var comment = 0;
-// var streams = [];
-// var fadeInterval = 1.6;
-// var symbolSize = 14;
-// var comments = [
-//                 "Why do women let go of bodies. Find a gym or call Jenny Craig. Gress",
-//                 "She has a prosthetic leg, and just flirting with everyone",
-//                 "OMG, such a fat pig",
-//                 "Why you're even building a career",
-//                 "You get fat these days",
-//                 "What did your husband think about this",
-//                 "You dress is so short, you shouln't wear it"
-//                 ];
-
 
 function preload() {
     eyeImg = loadImage('images/eye.png');
     whisper = loadSound("sound/whisper.mp3");
+
+    // whisper = new p5.AudioIn("sound/whisper.mp3");
+    // whisper.start();
+    // getAudioContext().resume();
+
+    // whisper = async () => {
+    //     try {
+    //         return await loadSound("sound/whisper.mp3");
+    //         console.log("LOAD SUCCESSFULLY!!");
+    //     } catch (e) {
+    //         console.log("LOAD FAIL!!", e);
+    //     }
+    // }
 }
 
 // Setup
@@ -38,19 +36,14 @@ function setup() {
     colorMode(RGB,255,255,255,1);
     // background(0);
 
-    // comments
-    // var y = 0;
-    // for (var i = 0; i <= height / symbolSize; i++) {
-    //     var stream = new Stream();
-    //     // stream.generateSymbols(random(-2000, 0), y);
-    //     stream.generateSymbols(random(0, 2000), y);
-    //     streams.push(stream);
-    //     y += symbolSize
-    // }
 
-    // whisper.play();
-    whisper.loop();
-    whisper.setVolume(0.5);
+
+    whisper.play();
+    // whisper.resume().then(() => {
+    //     console.log('Playback resumed successfully');
+    // });
+    // whisper.loop();
+    // whisper.setVolume(0.5);
      
     // Create Eyes
      for (i = 0; i < numEyes; i++) {    
@@ -131,64 +124,6 @@ function Eye(x, y) {
 }
 
 
-// function Symbol(x, y, speed, first, opacity) {
-//     this.x = x;
-//     this.y = y;
-//     // this.value;
-
-//     this.speed = speed;
-//     this.first = first;
-//     this.opacity = opacity;
-
-//     this.setToRandomSymbol = function(){
-//         // var a = round(random(0,comments.length-1))
-//         this.value = comments[comment%comments.length]
-//         comment = comment + 1;
-//         // console.log(a);
-//     }
-
-//     this.rain = function () {
-//         this.x = (this.x >= width) ? 0 : this.x += this.speed;
-//     }
-
-// }
-
-// function Stream() {
-//     this.symbols = [];
-//     this.speed = random(1,3); 
-
-//     this.generateSymbols = function (x, y) {
-//         var opacity = 255;
-//         var first = round(random(0, 4)) == 1;
-//         symbol = new Symbol(
-//             x,
-//             y,
-//             this.speed,
-//             first,
-//             opacity
-//         );
-//         symbol.setToRandomSymbol();
-//         this.symbols.push(symbol);
-
-//     }
-
-//     this.render = function () {
-        
-//         this.symbols.forEach(function (symbol) {
-//             if (symbol.first) {
-//                 fill(134, 128, 128, symbol.opacity);  // 134, 128, 128,
-//             } else {
-//                 fill(92, 79, 79, symbol.opacity);  // 92, 79, 79,
-//             }
-//             text(symbol.value, symbol.x, symbol.y);
-//             symbol.rain();
-//             // symbol.setToRandomSymbol();
-//         });
-//     }
-// }
-
-
-
 // Update Canvas
 function draw() {
     
@@ -201,15 +136,11 @@ function draw() {
 
 
     var target = createVector(mouseX, mouseY);
-    for (i = 0; i < numEyes; i++) { //
+    for (i = 0; i < numEyes; i++) {
         eyes[i].seek(target);
         eyes[i].display();
         eyes[i].update();
     }
-
-    // streams.forEach(function (stream) {
-    //     stream.render();
-    // });
 
 }
 
