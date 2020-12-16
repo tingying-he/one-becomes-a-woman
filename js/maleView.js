b3c29c5929599be6ef9f97e87c4e433517b93805
tingying-he <tingying.he@outlook.com>
2020-12-16 14:09:00 +0100

    loading

--- js/maleView.js
+++ js/maleView.js
@@ -13,3 +13,21 @@ btn.onclick=function(){
 // hover.onmouseout=function(){
 //         video.pause();
 // }
+
+function onReady(callback) {
+        var intervalId = window.setInterval(function() {
+          if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+          }
+        }, 1000);
+      }
+      
+      function setVisible(selector, visible) {
+        document.querySelector(selector).style.display = visible ? 'block' : 'none';
+      }
+      
+      onReady(function() {
+        setVisible('.page', true);
+        setVisible('#loading', false);
+      });
--- js/question.js
+++ js/question.js
@@ -39,3 +39,22 @@ nextBtn.onclick=function(){
 //       location.href="./selfieView-black-word.html"
 //     }
 //   }
+
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
