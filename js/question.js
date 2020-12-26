9e851a53e0d2c3f4810d818ce6b3e8deca1a3791
tingying-he <tingying.he@outlook.com>
2020-12-27 00:18:48 +0100

    delete useless file

--- js/question.js
+++ js/question.js
@@ -4,16 +4,15 @@ nextBtn.onclick=function(){
     var ipt=document.getElementById('ipt').value;
     console.log(ipt)
     if(ipt){
-        window.localStorage.setItem('val',ipt)
-        if (confirm("To continue your experience, you can choose OK to open your camera or Cancel to experience without camera.")) {
-            location.href="./selfieView-camera-word.html"
+        window.localStorage.setItem('val',ipt);
+        location.href="./selfieView.html";
+        if (confirm("To continue your experience, you can choose OK to open your camera or Cancel to experience without camera.")) {          
             window.localStorage.setItem('look',"./images/selfie/look-camera.gif");
          } else {
-            location.href="./selfieView-camera-word.html"
             window.localStorage.setItem('look',"./images/selfie/look-black.gif");
          }
     }else{
-        alert('Please enter a word')
+        alert('Please enter a word');
     }
 }
 
--- js/selfieView-black-skip.js
+++
@@ -1,6 +0,0 @@
-
-var btn=document.getElementById('btn');
-btn.onclick=function(){
-        location.href="./launch.html"
-   
-}
