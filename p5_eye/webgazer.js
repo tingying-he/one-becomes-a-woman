c017d798d99973a927aeff2f0ec02939832f483e
Tiannan Yang <tiannan@kth.se>
2021-01-13 16:27:35 +0800

    bigger red dot

--- p5_eye/webgazer.js
+++ p5_eye/webgazer.js
@@ -88295,8 +88295,8 @@ async function init(stream) {
   gazeDot.style.background = 'red';
   gazeDot.style.borderRadius = '100%';
   gazeDot.style.opacity = '0.7';
-  gazeDot.style.width = '10px';
-  gazeDot.style.height = '10px';
+  gazeDot.style.width = '50px';
+  gazeDot.style.height = '50px';
 
   // Add other preview/feedback elements to the screen once the video has shown and its parameters are initialized
   document.body.appendChild(videoElement);
