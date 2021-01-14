301c02cf12f40aca4a1ddb008ee58b787c23daa3
liubq7 <liubq97@gmail.com>
2021-01-14 10:43:51 +0800

    fix 404

--- js/train.js
+++ js/train.js
@@ -1,11 +1,13 @@
-webgazer.setGazeListener(function(data, elapsedTime) {
-  if (data == null) {
+webgazer
+  .setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
       return;
-  }
-  var xprediction = data.x; //these x coordinates are relative to the viewport
-  var yprediction = data.y; //these y coordinates are relative to the viewport
-  console.log(elapsedTime); //elapsed time is based on time since begin was called
-}).begin();
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(elapsedTime); //elapsed time is based on time since begin was called
+  })
+  .begin();
 
 window.saveDataAcrossSessions = true;
 
@@ -14,11 +16,16 @@ var nextBtn = document.getElementById("next");
 
 const eyes = document.getElementsByClassName("eye");
 for (let i = 0; i < eyes.length; i++) {
-  eyes[i].onclick = function() {
+  eyes[i].onclick = function () {
     this.src = "./images/male/open.gif";
-    eyesNum ++
-    if(eyesNum == 5){
+    eyesNum++;
+    if (eyesNum == 5) {
       nextBtn.disabled = false;
     }
-  }
+  };
 }
+
+var btn = document.getElementById("next");
+btn.onclick = function () {
+  location.href = "./maleView_eye.html";
+};
